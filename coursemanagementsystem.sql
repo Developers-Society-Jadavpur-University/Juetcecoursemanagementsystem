61afe154331de30da73cbd3076c3e5765cca85d8
ayan456 <ayanbiswas184@gmail.com>
2020-03-20 15:43:01 +0530

    sql file modified for previous commit

--- coursemanagementsystem.sql
+++ coursemanagementsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 25, 2020 at 11:41 PM
+-- Generation Time: Mar 20, 2020 at 03:42 PM
 -- Server version: 5.7.29-0ubuntu0.18.04.1
 -- PHP Version: 7.2.24-0ubuntu0.18.04.3
 
@@ -19,6 +19,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `coursemanagementsystem`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_tokens`
+--
+
+CREATE TABLE `auth_tokens` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `auth_type` varchar(255) NOT NULL,
+  `selector` text NOT NULL,
+  `token` longtext NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `expires_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `auth_tokens`
+--
+
+INSERT INTO `auth_tokens` (`id`, `user_email`, `auth_type`, `selector`, `token`, `created_at`, `expires_at`) VALUES
+(65, 'ayanbiswas184@gmail.com', 'password_reset', 'd5e7c3ee59c9cb44', '$2y$10$WD1TakwZDhhyUasycK7aM.6bg13Qq2Uc/XlyohgpLIAh70xMetmVq', '2020-03-03 15:39:05', '2020-03-03 16:39:05'),
+(73, 'ayanbiswas184@gmail.com', 'account_verify', '81edee3f6c8a5ffc', '$2y$10$7X2DGSRHeAOYEmexP.HG8us/W.wMibh05wTjkB3uw0t.MWBkL1COG', '2020-03-15 08:55:08', '2020-03-15 09:55:08');
 
 -- --------------------------------------------------------
 
@@ -66,7 +90,7 @@ CREATE TABLE `student_parentdb` (
 --
 
 INSERT INTO `student_parentdb` (`Roll_no`, `Father_name`, `Mother_name`, `Father_primary_mobno`, `Father_secondary_mobno`, `Father_email`, `Mother_primary_mobno`, `Mother_secondary_mobno`, `Mother_email`, `parents_primaryaddress`) VALUES
-(1910701018, 'fater_name', 'mother_name', '4585245689', '4585458956', 'sample0967@gmail.com', '7854859658', '4585785485', 'sample@gmail.com', 'same as my home address');
+(1910701012, 'SAMARESH BISWAS', 'SIKHA BISWAS', '9474828561', '9330303814', 'samareshbiswas0967@gmail.com', '9434936752', '7003924293', 'sikhabiswas011@gmail.com', 'same as my home address');
 
 -- --------------------------------------------------------
 
@@ -89,11 +113,71 @@ CREATE TABLE `student_userdata` (
 --
 
 INSERT INTO `student_userdata` (`roll_no`, `Full_name`, `Course_code`, `reg_status`, `course_name`, `department`, `faculty`) VALUES
-(1910701018, 'sample', 'BETC1923', 'N', 'Bachelor of Engineering', 'Electronics and Telecommunication Engineering', 'Faculty of Engineering and Technology');
+(1910701012, 'AYAN BISWAS', 'BETC1923', 'N', 'Bachelor of Engineering', 'Electronics and Telecommunication Engineering', 'Faculty of Engineering and Technology');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studusers_logindata`
+--
+
+CREATE TABLE `studusers_logindata` (
+  `Roll_no` varchar(12) DEFAULT NULL,
+  `Full_name` varchar(60) DEFAULT NULL,
+  `mobile_no` varchar(12) DEFAULT NULL,
+  `email_id` varchar(70) DEFAULT NULL,
+  `Password_login` varchar(70) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `studusers_logindata`
+--
+
+INSERT INTO `studusers_logindata` (`Roll_no`, `Full_name`, `mobile_no`, `email_id`, `Password_login`) VALUES
+('001910701012', 'AYAN BISWAS', '8777673298', 'ayanbiswas184@gmail.com', '9990452b5fb2a693a3f100baa93fc891');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `headline` varchar(255) DEFAULT NULL,
+  `bio` text,
+  `profile_image` varchar(255) NOT NULL DEFAULT '_defaultUser.png',
+  `verified_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `last_login_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `first_name`, `last_name`, `gender`, `headline`, `bio`, `profile_image`, `verified_at`, `created_at`, `updated_at`, `deleted_at`, `last_login_at`) VALUES
+(0, 'supahot', 'supa@hot.com', '$2y$10$jhIOk4NVdBile/NwhAU9We/f0aoohx.cG9CizmIALRz0aCKJa5s6a', 'Supahot', 'Soverysupahot', 'm', 'Headline of a supa hot user', 'This is the bio of a supa hot user. Now i will say needless stuff to make this longer so this looks like a bio and not anything other than a bio.', '_defaultUser.png', '2020-03-03 15:22:48', '2020-03-03 15:22:48', '2020-03-15 08:58:23', NULL, NULL),
+(31, 'ayan456', 'ayanbiswas184@gmail.com', '$2y$10$BgvbHUn8pT/HOtuClYeEV..Qawrl4GxqBj1QvSig2PYWDJfICc1M6', 'AYAN', 'BISWAS', 'm', 'Hi i am good student', 'hello', '_defaultUser.png', '2020-03-03 15:23:45', '2020-03-03 15:23:45', '2020-03-15 09:00:20', NULL, '2020-03-15 09:00:20');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `auth_tokens`
+--
+ALTER TABLE `auth_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- Indexes for table `course_info`
@@ -118,6 +202,35 @@ ALTER TABLE `student_userdata`
   ADD KEY `Course_code_2` (`Course_code`),
   ADD KEY `Course_code_4` (`Course_code`);
 
+--
+-- Indexes for table `studusers_logindata`
+--
+ALTER TABLE `studusers_logindata`
+  ADD UNIQUE KEY `Roll_no` (`Roll_no`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `id` (`id`,`username`,`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `auth_tokens`
+--
+ALTER TABLE `auth_tokens`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- Constraints for dumped tables
 --
