4fe4aa933e5170a243fa15ecf0dd99d742a0dce8
Arijit Saha <arijitfeb01@gmail.com>
2020-04-13 20:59:34 +0530

    login.inc.php configured and new etce_database.sql added

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -9,18 +9,18 @@
         $password = $_POST['pwdl'];
 
         if(empty($username)||empty($rollno)||empty($password)){
-            header("Location: ../login.php?error=emptyfields");
+            header("Location: ../login.php?login-type=student&error=emptyfields");
             exit();
         }
         else if(!filter_var($username,FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-z A-Z]*$/",$username)){
-            header("Location: ../login.php?error=invalidusername");
+            header("Location: ../login.php?login-type=student&error=invalidusername");
             exit();
         }
         else{
             $sql = "SELECT * FROM users WHERE uname=? OR email=? AND rno=?;";
             $stmt = mysqli_stmt_init($conn);
             if(!mysqli_stmt_prepare($stmt,$sql)){
-                header("Location: ../login.php?error=sqlerror");
+                header("Location: ../login.php?login-type=student&error=sqlerror");
                 exit();
             }
             else{
@@ -30,7 +30,7 @@
                 if($row = mysqli_fetch_assoc($result)){
                     $pwdCheck = password_verify($password,$row['pwd']);
                     if($pwdCheck == false){
-                        header("Location: ../login.php?error=wrongpassword");
+                        header("Location: ../login.php?login-type=student&error=wrongpassword");
                         exit();
                     }
                     else if($pwdCheck == true){
@@ -51,6 +51,55 @@
         }
         mysqli_stmt_close($stmt);
         mysqli_close($conn);
+    }else if(isset($_POST['login-staff-submit'])){
+
+        require "dbh.inc.php";
+
+        $username = $_POST['uname'];
+        $rollnumber = $_POST['rollno'];
+        $role = $_POST['role'];
+        $password = $_POST['pwd'];
+
+        if(empty($username)||empty($rollnumber)||empty($role)||empty($password)){
+            header("Location: ../login.php?login-type=staff&error=emptyfields");
+            exit();
+        }
+        else if(!filter_var($username,FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-z A-Z]*$/",$username)){
+            header("Location: ../login.php?login-type=staff&error=invalidusername");
+            exit();
+        } else{
+            $sql = "SELECT * FROM users_staff WHERE uname=? OR email=? AND rno=?;";
+            $stmt = mysqli_stmt_init($conn);
+            if(!mysqli_stmt_prepare($stmt,$sql)){
+                header("Location: ../login.php?login-type=staff&error=sqlerror");
+                exit();
+            }
+            else{
+                mysqli_stmt_bind_param($stmt,"sss",$username,$username,$rollno);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+                if($row = mysqli_fetch_assoc($result)){
+                    $pwdCheck = password_verify($password,$row['pwd']);
+                    if($pwdCheck == false){
+                        header("Location: ../login.php?login-type=staff&error=wrongpassword");
+                        exit();
+                    }else if($pwdCheck == true){
+                        session_start();
+                        $_SESSION['uid']=$row['uname'];
+                        $_SESSION['roll']=$row['rno'];
+                        $_SESSION['role']=$row['role'];
+                        if($_SESSION['role'] == 'admin'){
+                            header("Location: ../admindashboard.php?login=success");
+                            exit();
+                        }else if($_SESSION['role'] == 'faculty'){
+                            header("Location: ../facultydashboard.php?login=success");
+                            exit();
+                        }
+                    }
+                }
+            }
+        }
+
     }
     else{
         header("Location: ../login.php");
